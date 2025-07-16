63d460b677fcec082771c4a0cd68be27d4f4fa55
Felipe Carvalho <104037790+LippeTK@users.noreply.github.com>
2025-07-16 16:45:42 -0300

    feat: add functions on form to save inputs and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Form from './components/layout/Form'
+import { useState } from 'react'
+
+import type { ITask } from './interfaces/Task';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
 
   return (
     <>
       <Header/>
       <main className="main-container">
         <h1>O que você gostaria de fazer?</h1>
-        <Form/>
+        <Form btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList}/>
       </main>
       <Footer/>
     </>
--- src/components/layout/Form.tsx
+++ src/components/layout/Form.tsx
@@ -1,23 +1,56 @@
 import './Form.css'
 
-function Form(){
+import type { ITask } from '../../interfaces/Task'
+import { useState, type ChangeEvent, type FormEvent } from 'react'
+
+interface Props{
+    btnText:string,
+    taskList: ITask[]
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+}
+
+function Form({btnText, taskList, setTaskList}:Props){
+
+    const [title, setTitle] = useState<string>("")
+    const [category, setCategory] = useState<string>("work")
+
+    const addTask = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const id = Math.floor(Math.random() * 10000)
+        
+        const newTask: ITask = {id, title, category}
+
+        setTaskList!([...taskList, newTask])
+        setTitle("")
+        setCategory("")
+
+        console.log(taskList)
+    }
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        if (e.target.name === "title"){
+            setTitle(e.target.value)
+        }else{
+            setCategory(e.target.value)
+        }
+    }
     return(
         <div>
-            <form className="form">
+            <form onSubmit={addTask} className="form">
                 <div className='input-container'>
                     <label>Título:</label>
-                    <input type="text" name="title" />
+                    <input type="text" name="title" value={title} onChange={handleChange} />
                 </div>
                 <div className='input-container'>
                     <label>Categoria:</label>
-                    <select name="category">
+                    <select name="category" onChange={handleChange}>
                         <option value="work">Trabalho</option>
                         <option value="study">Estudos</option>
                         <option value="health">Saúde</option>
                         <option value="personal">Pessoal</option>
                     </select>
                 </div>
-                <input type="submit" value="Cadastrar"/>
+                <input type="submit" value={btnText}/>
             </form>
         </div>
     )
