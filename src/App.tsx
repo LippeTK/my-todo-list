bfe2f49194f95c03278c385ee22376ac5ee8e243
Felipe Carvalho <104037790+LippeTK@users.noreply.github.com>
2025-07-17 12:33:27 -0300

    add delete function for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
 
-
+  const deleteTask = (id:number) => {
+    setTaskList(taskList.filter((task) =>{
+      return task.id !== id
+    }))
+  }
   return (
     <>
       <Header/>
       <main className="main-container">
         <h1>O que você gostaria de fazer?</h1>
         <Form btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList}/>
-        <TaskList taskList={taskList}/>
+        <TaskList taskList={taskList} handleDelete={deleteTask}/>
       </main>
       <Footer/>
     </>
